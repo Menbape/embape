0af9a6d0b33113a77f27ae70f550ea1eaeebdee5
Menbape <106734302+Menbape@users.noreply.github.com>
2022-06-08 09:33:38 +0200

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestOpen, function (sprite, location) {
+    game.over(true)
+})
 let mySprite = sprites.create(assets.image`embape`, SpriteKind.Player)
 controller.moveSprite(mySprite, 100, 100)
 scene.setBackgroundImage(img`
@@ -123,4 +126,6 @@ scene.setBackgroundImage(img`
     9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
     `)
 tiles.setCurrentTilemap(tilemap`level1`)
+tiles.placeOnRandomTile(mySprite, assets.tile`transparency16`)
 scene.cameraFollowSprite(mySprite)
+info.startCountdown(20)
